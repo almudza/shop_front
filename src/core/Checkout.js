d4b016374071db3cad05925c0aeb97412fc426f3
almudza <alifulmudzakir@gmail.com>
2019-10-27 11:17:41 +0700

    store address using let outsite function because data already inside the buy function

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -54,6 +54,9 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
         )
     }
 
+    // <!-- Initialitaion Address -->
+    let deliveryAddress = data.address
+
     const buy = () => {
         setData({ loading: true })
         // send the nonce to your server
@@ -89,7 +92,7 @@ const Checkout = ({ products, setRun = f => f, run = undefined }) => {
                             products: products,
                             transaction_id: response.transaction.id,
                             amount: response.transaction.amount,
-                            address: data.address,
+                            address: deliveryAddress,
                         }
 
                         createOrder(userId, token, createOrderData)
